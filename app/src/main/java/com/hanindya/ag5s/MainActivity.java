8d0f2095a6ae9943f82b5ce6c0212d875ca3b00c
ridwan0806 <ridwanm32@gmail.com>
2023-08-06 11:41:01 +0700

    add filter history

--- app/src/main/java/com/hanindya/ag5s/MainActivity.java
+++ app/src/main/java/com/hanindya/ag5s/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,12 +30,20 @@ import com.hanindya.ag5s.Activity.MenuActivity;
 import com.hanindya.ag5s.Activity.SuppliesActivity;
 import com.hanindya.ag5s.Activity.UserActivity;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
-    ConstraintLayout food,supplies,cost,cashier,userPage,logout;
-    TextView userName,userBranch;
     FirebaseUser firebaseUser;
     DatabaseReference root,dbUser;
     String userId;
+    TextView userName,userBranch;
+
+    ConstraintLayout food,supplies,cost,cashier,information,userPage,logout;
+
+    TextView infoType,infoStart,infoEnd;
+    String transactionType = "";
+    String transactionStartDate = "";
+    String transactionEndDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +70,11 @@ public class MainActivity extends AppCompatActivity {
         
         food = findViewById(R.id.ic_food);
         supplies = findViewById(R.id.ic_supplies);
+
         cost = findViewById(R.id.ic_cost);
         cashier = findViewById(R.id.ic_cashier);
+
+        information = findViewById(R.id.ic_information);
         userPage = findViewById(R.id.ic_user);
         logout = findViewById(R.id.ic_logout);
 
@@ -80,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
         cashier.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, CashierActivity.class);
             startActivity(i);
+        });
+
+        information.setOnClickListener(view -> {
+            filterInfoType();
         });
 
         userPage.setOnClickListener(view -> {
@@ -111,6 +131,118 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void filterInfoType() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Filter Pencarian");
+
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        View layout = layoutInflater.inflate(R.layout.dialog_filter_history,null);
+        builder.setView(layout);
+
+        infoType = layout.findViewById(R.id.cmbHistoryFilterType);
+        infoStart = layout.findViewById(R.id.cmbHistoryStartDate);
+        infoEnd = layout.findViewById(R.id.cmbHistoryEndDate);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        infoType.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this,infoType);
+            popupMenu.getMenuInflater().inflate(R.menu.menu_history_type,popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    int itemId = menuItem.getItemId();
+                    if (itemId == R.id.history_uang_masuk){
+                        transactionType = "pemasukan";
+                        infoType.setText("Uang Masuk (Penjualan)");
+                    } else if (itemId == R.id.history_uang_keluar){
+                        transactionType = "pengeluaran";
+                        infoType.setText("Uang Keluar (Belanja,Biaya Lainnya)");
+                    } else {
+                        transactionType = "";
+                    }
+                    return true;
+                }
+            });
+            popupMenu.show();
+        });
+
+        infoStart.setOnClickListener(view -> {
+            DatePickerDialog dialogStart = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+
+                    String monthString = "";
+                    if (month < 10){
+                        monthString = "0"+month;
+                    } else {
+                        monthString = String.valueOf(month);
+                    }
+
+                    String dayString = "";
+                    if (day < 10){
+                        dayString = "0"+day;
+                    } else {
+                        dayString = String.valueOf(day);
+                    }
+
+                    infoStart.setText(year+"-"+monthString+"-"+dayString);
+                    transactionStartDate = infoStart.getText().toString();
+                }
+            }, year, month, day);
+            dialogStart.show();
+        });
+
+        infoEnd.setOnClickListener(view -> {
+            DatePickerDialog dialogEnd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+
+                    String monthString = "";
+                    if (month < 10){
+                        monthString = "0"+month;
+                    } else {
+                        monthString = String.valueOf(month);
+                    }
+
+                    String dayString = "";
+                    if (day < 10){
+                        dayString = "0"+day;
+                    } else {
+                        dayString = String.valueOf(day);
+                    }
+
+                    infoEnd.setText(year+"-"+monthString+"-"+dayString);
+                    transactionEndDate = infoEnd.getText().toString();
+                }
+            }, year, month, day);
+            dialogEnd.show();
+        });
+
+        builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.setPositiveButton("Cari", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.show();
     }
 
     private void logout() {
